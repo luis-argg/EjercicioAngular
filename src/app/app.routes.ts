7e15e3058f18a48806acb403d8b02c26c10d10f5
LuisArgueta <largueta-2025599@kinal.edu.gt>
2026-08-20 16:54:28 -0600

    Avanze del ejercicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () => import('./feature/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./feature/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./feature/register/register').then((m) => m.Register),
+  },
+  { path: '**', redirectTo: 'home' },
+];
--- src/app/feature/home/home.ts
+++ src/app/feature/home/home.ts
@@ -1,9 +1,59 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+interface Album {
+  titulo: string;
+  anio: number;
+  imagen: string;
+}
+
+interface Integrante {
+  nombre: string;
+  instrumento: string;
+}
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  protected readonly heroImagen =
+    'https://live-production.wcms.abc-cdn.net.au/3769560fef175d8bcbc7adba4c3fbcbb?impolicy=wcms_watermark_abc&cropH=1125&cropW=1998&xPos=1&yPos=0&width=862&height=485&imformat=generic';
+
+  protected readonly albums: Album[] = [
+    {
+      titulo: 'Is This It',
+      anio: 2001,
+      imagen:
+        'https://m.media-amazon.com/images/I/91PbdrEjhaL._UF1000,1000_QL80_.jpg',
+    },
+    {
+      titulo: 'Room on Fire',
+      anio: 2003,
+      imagen:
+        'https://i.discogs.com/qeZqSMPWDjs1HISu_GI8z-BAfLSCBwo0V1_dPSqOhlE/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc5NTUz/MDUtMTQ1MjQzNzA1/NC0yOTA0LnBuZw.jpeg',
+    },
+    {
+      titulo: 'First Impressions of Earth',
+      anio: 2006,
+      imagen:
+        'https://images.genius.com/61a37cadd18c410909414246027a4d0a.1000x1000x1.png',
+    },
+    {
+      titulo: 'The New Abnormal',
+      anio: 2020,
+      imagen:
+        'https://www.guioteca.com/los-2000/files/2021/04/81xesqe7UML._SL1500_.jpg',
+    },
+  ];
+
+  protected readonly integrantes: Integrante[] = [
+    { nombre: 'Julian Casablancas', instrumento: 'Voz' },
+    { nombre: 'Nick Valensi', instrumento: 'Guitarra' },
+    { nombre: 'Albert Hammond Jr.', instrumento: 'Guitarra' },
+    { nombre: 'Nikolai Fraiture', instrumento: 'Bajo' },
+    { nombre: 'Fabrizio Moretti', instrumento: 'Batería' },
+  ];
+}
